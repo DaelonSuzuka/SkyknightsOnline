48021cf8f033c627cca502e2388ee4f4bf7ecfb5
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-09-20 06:47:55 -0400

    Added ability to create new debug labels at runtime

--- src/ui/hud/Debug.gd
+++ src/ui/hud/Debug.gd
@@ -3,5 +3,16 @@ extends PanelContainer
 onready var Throttle = $VBox/Throttle
 onready var Velocity = $VBox/Velocity
 
+var fields = {}
+
 func _ready():
     pass
+
+func set_field(field, value):
+    if !(field in fields):
+        var label = Label.new()
+        fields[field] = label
+        $VBox.add_child(label)
+
+    fields[field].text = field + ': ' + value
+    
