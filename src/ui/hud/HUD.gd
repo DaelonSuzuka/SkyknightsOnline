3bafa31166a6f71f8474b9d6186aed67025d546c
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-09-25 02:26:51 -0400

    Removed SeatingDiagram and replaced reload indicator textures

--- src/ui/hud/HUD.gd
+++ src/ui/hud/HUD.gd
@@ -2,17 +2,15 @@ extends CanvasLayer
 
 onready var Radial = $Radial
 onready var Debug = $Debug
-onready var SeatingDiagram = $SeatingDiagram
 onready var WeaponInfo = $WeaponInfo
 onready var Crosshair = $Crosshair
 onready var ReloadIndicator = $ReloadIndicator
 onready var DebugOverlay = $DebugOverlay
-onready var Chat = $ChatBox
+# onready var Chat = $ChatBox
 
 func _ready():
-    SeatingDiagram.hide()
     WeaponInfo.hide()
     Crosshair.hide()
     ReloadIndicator.hide()
-    Chat.hide()
+    # Chat.hide()
     # Debug.hide()
