8eefeb189c9233cb0044be91f223d19c85471243
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-09-29 07:15:49 -0400

    BIg overhaul of scenes

--- src/scenes/TestWorld.gd
+++
@@ -1,8 +0,0 @@
-extends Spatial
-
-func _ready():
-    #$Player.enter_ship($Reaver)
-    pass
-
-func _process(delta):
-   pass
---
+++ src/scenes/VRTraining/VRTraining.gd
@@ -0,0 +1,8 @@
+extends Spatial
+
+func _ready():
+    #$Player.enter_ship($Reaver)
+    pass
+
+func _process(delta):
+   pass
