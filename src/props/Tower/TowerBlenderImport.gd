d16ab58f2e872964b71535d60e58f27851d46840
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-10-03 10:50:32 -0400

    Added biolab and tower props

---
+++ src/props/Tower/TowerBlenderImport.gd
@@ -0,0 +1,24 @@
+tool
+extends EditorScenePostImport
+
+func post_import(scene):
+    scene.name = 'Model'
+
+    for node in scene.get_children():
+        var material = SpatialMaterial.new()
+        material.albedo_texture = load('res://src/props/biolab/Deck.png')
+        node.set_surface_material(0, material)
+
+    # var deck = SpatialMaterial.new()
+    # deck.albedo_texture = load('res://src/props/biolab/Deck.png')
+    # scene.get_node('Deck').set_surface_material(0, deck)
+
+    # var dome = SpatialMaterial.new()
+    # dome.albedo_texture = load('res://src/props/biolab/Dome.png')
+    # scene.get_node('Dome').set_surface_material(0, dome)
+
+    # var pads = SpatialMaterial.new()
+    # pads.albedo_texture = load('res://src/props/biolab/LandingPads.png')
+    # scene.get_node('LandingPads').set_surface_material(0, pads)
+
+    return scene
