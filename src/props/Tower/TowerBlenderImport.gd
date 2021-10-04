cbc8760a1c58feaf8bcec5f6da4fb0022ddaf05e
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-10-03 23:23:25 -0400

    Cleaned up Tower

--- src/props/Tower/TowerBlenderImport.gd
+++ src/props/Tower/TowerBlenderImport.gd
@@ -9,16 +9,4 @@ func post_import(scene):
         material.albedo_texture = load('res://src/props/biolab/Deck.png')
         node.set_surface_material(0, material)
 
-    # var deck = SpatialMaterial.new()
-    # deck.albedo_texture = load('res://src/props/biolab/Deck.png')
-    # scene.get_node('Deck').set_surface_material(0, deck)
-
-    # var dome = SpatialMaterial.new()
-    # dome.albedo_texture = load('res://src/props/biolab/Dome.png')
-    # scene.get_node('Dome').set_surface_material(0, dome)
-
-    # var pads = SpatialMaterial.new()
-    # pads.albedo_texture = load('res://src/props/biolab/LandingPads.png')
-    # scene.get_node('LandingPads').set_surface_material(0, pads)
-
     return scene
