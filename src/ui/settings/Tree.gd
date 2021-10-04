c9cf6a3ae1fd4890b7e45942fc82c66ef8e9388f
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-10-04 02:47:46 -0400

    Finally fixed the stupid keybinds menu

--- src/ui/settings/Tree.gd
+++ src/ui/settings/Tree.gd
@@ -7,15 +7,16 @@ func _ready():
     set_column_titles_visible(true)
     set_column_title(0, 'Action')
     set_column_title(1, 'Key')
-    # set_column_clip_content(0, true)
-#    connect('item_edited', self, '_action_edited')
-    var tex = load('res://icon.png')
+
     for action in InputManager.actions:
         var item = create_item()
         item.set_text(0, action)
-        item.add_button(1, tex)
-#        if 'key' in InputManager.actions[action]:
-#            item.set_text(1, InputManager.actions[action]['key'])
+        if 'key' in InputManager.actions[action]:
+            var s = OS.get_scancode_string(InputManager.actions[action]['key'])
+            item.set_text(1, s)
+        if 'mouse' in InputManager.actions[action]:
+            var s = 'Mouse ' + str(InputManager.actions[action]['mouse'])
+            item.set_text(1, s)
     connect('button_pressed', self, '_button_pressed')
     
 func _button_pressed(item, col, id):
