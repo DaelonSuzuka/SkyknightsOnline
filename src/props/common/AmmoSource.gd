222ccb8db1a821a8442ddef9ef89f6636070f1db
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-10-04 00:46:42 -0400

    AmmoSource node is now interactable in the editor, and correctly gives ammo to ships

--- src/props/common/AmmoSource.gd
+++ src/props/common/AmmoSource.gd
@@ -1,24 +1,52 @@
+tool
 class_name AmmoSource extends Area
 
+export(float) var trigger_delay = 5.0
+
+export(Shape) var shape setget set_shape
+func set_shape(new_shape):
+    shape = new_shape
+    if is_inside_tree():
+        $CollisionShape.shape = new_shape
+
+export(Vector3) var offset setget set_offset
+func set_offset(new_offset):
+    offset = new_offset
+    if is_inside_tree():
+        $CollisionShape.transform.origin = new_offset
+
+export(Array, NodePath) var ignore
+var ignored_bodies = []
 
 var targets = []
 
-
 func _ready():
-    connect('body_entered', self, 'on_body_entered')
-    connect('body_exited', self, 'on_body_exited')
+    if shape:
+        $CollisionShape.shape = shape
+        $CollisionShape.transform.origin = offset
 
+    if not Engine.editor_hint:
+        for path in ignore:
+            ignored_bodies.append(get_node(path))
+
+        connect('body_entered', self, 'on_body_entered')
+        connect('body_exited', self, 'on_body_exited')
 
 func on_body_entered(body: Node):
-    targets.append(body)
+    if !(body in ignored_bodies):
+        targets.append(body)
 
 func on_body_exited(body: Node):
-    targets.erase(body)
+    if body in targets:
+        targets.erase(body)
 
 var time = 0.0
 func _physics_process(delta):
     time += delta
-    if time >= 5:
-        time = 0
-        print(targets)
+    if time < trigger_delay:
+        return
+    time = 0
     
+    for target in targets:
+        if target.has_method('give_ammo'):
+            target.give_ammo()
