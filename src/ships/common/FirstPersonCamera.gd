e3c03a2ce4724e265c8e78d9cacc22a9e0cf1a5a
Daelon Suzuka <daelonsuzuka@gmail.com>
2021-12-29 23:12:04 -0500

    Fixed variable shadowing issue

--- src/ships/common/FirstPersonCamera.gd
+++ src/ships/common/FirstPersonCamera.gd
@@ -6,20 +6,20 @@ var yaw_limit = Vector2(-PI/2, PI/2)
 var pitch_limit = Vector2(-PI/2, PI/5)
 
 func get_pos():
-    return $InnerGimbal/CameraPos
+	return $InnerGimbal/CameraPos
 
 func reset():
-    transform.basis = Basis()
-    $InnerGimbal.transform.basis = Basis()
+	transform.basis = Basis()
+	$InnerGimbal.transform.basis = Basis()
 
-func freelook(pos):
-    yaw(-pos.x)
-    pitch(pos.y)
+func freelook(_pos):
+	yaw(-_pos.x)
+	pitch(_pos.y)
 
 func yaw(value):
-    rotate_object_local(Vector3.UP, value)
-    rotation.y = clamp(rotation.y, yaw_limit.x, yaw_limit.y)
+	rotate_object_local(Vector3.UP, value)
+	rotation.y = clamp(rotation.y, yaw_limit.x, yaw_limit.y)
 
 func pitch(value):
-    $InnerGimbal.rotate_object_local(Vector3.RIGHT, value)
-    $InnerGimbal.rotation.x = clamp($InnerGimbal.rotation.x, pitch_limit.x, pitch_limit.y)
+	$InnerGimbal.rotate_object_local(Vector3.RIGHT, value)
+	$InnerGimbal.rotation.x = clamp($InnerGimbal.rotation.x, pitch_limit.x, pitch_limit.y)
