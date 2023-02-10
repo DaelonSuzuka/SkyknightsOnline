0de59fb6d25ce24afe8dcac2fe7c3b5f2ca489ce
David Kincaid <daelonsuzuka@gmail.com>
2023-02-10 04:20:04 -0500

    Updated City

--- src/scenes/City/CityBlenderImport.gd
+++ src/scenes/City/CityBlenderImport.gd
@@ -7,29 +7,27 @@ var tex_path = mat_path + '%s.png'
 
 func _post_import(scene):
 	scene.name = 'Terrain'
+	scene.scale *= 10
+	scene.position = Vector3(-10000, 0, 10000)
 
-	var material
-	var node
-
-	var ap = scene.get_node('AnimationPlayer')
-	scene.remove_child(ap)
-	ap.queue_free()
-
-	# node = scene.get_node('Mesh')
 	for child in scene.get_children():
 		child.name = child.name.capitalize()
 
-		if FileAccess.file_exists(res_path % (child.name + 'TerrainMaterial')):
-			material = load(res_path % (child.name + 'TerrainMaterial'))
-		else:
-			material = StandardMaterial3D.new()
-			print(material)
-			# material.albedo_color = Color.GRAY
-			ResourceSaver.save(res_path % (child.name + 'TerrainMaterial'), material)
+	var node
 
-		child.set_surface_override_material(0, material)
+	node = scene.get_node('Buildings')
+	node.create_trimesh_collision()
+	node.set_surface_override_material(0, load('res://src/scenes/City/CityBuildingShader.tres'))
 
-		child.create_trimesh_collision()
-		child.get_node('Mesh_col').hide()
+	node = scene.get_node('Domain')
+	node.create_trimesh_collision()
+	node.set_surface_override_material(0, load('res://src/scenes/City/CityBuildingShader.tres'))
+
+	node = scene.get_node('Blocks')
+	node.create_trimesh_collision()
+	node.set_surface_override_material(0, load('res://src/scenes/City/CityBuildingShader.tres'))
+
+	node = scene.get_node('River')
+	node.hide()
 
 	return scene
